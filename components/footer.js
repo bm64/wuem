884a730795680eb56c7c90307a129d08bc0acf6d
Mateusz Bernacik <mat.bernacik@gmail.com>
2019-09-19 21:28:38 +0200

    New footer

--- components/footer.js
+++ components/footer.js
@@ -1,83 +1,48 @@
-import styles from '../styles/footer.module.scss';
+import styles from "../styles/footer.module.scss";
 import {
   FaGooglePlusG,
   FaFacebookF,
   FaTwitter,
   FaLinkedinIn,
-  FaMapMarkedAlt,
-} from 'react-icons/fa';
+  FaMapMarkedAlt
+} from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className={styles.container}>
-      <div className={styles.footer}>
-        <div className={styles.footer__contact}>
-          <p>Kontakt</p>
-          <button type="button" className={styles.footer__button}>
-            Zadzwoń teraz
-          </button>
-          <button type="button" className={styles.footer__button}>
-            Wyślij wiadomość
-          </button>
-          <div className={styles.icons}>
-            <button className={styles.icon__button}>
-              <FaGooglePlusG />
-            </button>
-            <button className={styles.icon__button}>
-              <FaFacebookF />
-            </button>
-            <button className={styles.icon__button}>
-              <FaTwitter />
-            </button>
-
-            <button className={styles.icon__button}>
-              <FaLinkedinIn />
-            </button>
+    <div className={styles.footer}>
+      <div className={styles.footer__top}>
+        <hr className={styles.footer__line} />
+        <img src="/static/footer_logo2.png" />
+        <hr className={styles.footer__line} />
+      </div>
+      <div className={styles.footer__mid}>
+        <div className={styles.footer__left}>
+          <h1>
+            biuro@wuemfinanse.pl <br />
+            +48 509 755 700
+          </h1>
+        </div>
+        <hr />
+        <div className={styles.footer__right}>
+          <div className={styles.list__container}>
+            <h2>Ubezpieczenia</h2>
+            <ul>
+              <li>Ubezpieczenia pojazdów</li>
+              <li>Ubezpieczenia zdrowotne</li>
+              <li>Ubezpieczenia turystyczne</li>
+              <li>Ubezpieczenia nieruchomości</li>
+            </ul>
+          </div>
+          <div>
+            <h2>Finanse</h2>
+            <ul>
+              <li>Kredyt</li>
+              <li>Leasing</li>
+            </ul>
           </div>
         </div>
-        <div className={styles.footer__address}>
-          <FaMapMarkedAlt className={styles.footer__address_icon} />
-          <span>
-            ul.Rynek 27 I piętro,
-            <br /> 39-300 Mielec
-          </span>
-        </div>
-        <div className={styles.footer__logo}>
-          <img className={styles.logo} src="/static/footer_logo.png" />
-        </div>
-        <div className={styles.footer__navigation}>
-          <p>Nawigacja</p>
-          <ul style={{listStyleType: 'none'}}>
-            <li>
-              <a>Start</a>
-            </li>
-            <li>
-              <a>Ubezpiecznia</a>
-            </li>
-            <li>
-              <a>Kredyt i Leasing</a>
-            </li>
-            <li>
-              <a>Kontakt</a>
-            </li>
-            <li>
-              <a>RODO</a>
-            </li>
-          </ul>
-        </div>
       </div>
-      <div className={styles.copyright}>
-        © 2019 Copyright: &nbsp;
-        <a
-          href="http://www.wuemfinanse.pl"
-          style={{
-            color: '#a69c68',
-            textDecoration: 'none',
-          }}>
-          {' '}
-          WueMfinanse.pl
-        </a>
-      </div>
+      <div className={styles.footer__bottom}>Copyright © 2019 WueMFinanse</div>
     </div>
   );
 }
