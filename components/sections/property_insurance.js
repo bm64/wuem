da41cda97f7c551b6b854922c32913b19785acea
Kamil Jakubus <kamiljkbz@protonmail.com>
2019-09-21 04:57:06 +0200

    Add initial property insurance content

--- components/sections/property_insurance.js
+++ components/sections/property_insurance.js
@@ -3,6 +3,8 @@ import React, {useState} from 'react';
 import styles from '../../styles/sections/property_insurance.module.scss';
 
 import withScrollIn from '../../animations/withScrollIn';
+import withFadeIn from '../../animations/withFadeIn';
+
 import {
   withLeftSlideIn,
   withRightSlideIn,
@@ -19,7 +21,11 @@ function PropertyInsurance() {
         <img src={'/static/property_insurance_img.jpeg'} />
       </div>
       <div className={styles.fixedSection}>
-        <div className={styles.cards}></div>
+        <div className={styles.cards}>
+          <HouseInsurance />
+          <FlatInsurance />
+          <HouseInBuildInsurance />
+        </div>
       </div>
     </>
   );
@@ -45,31 +51,40 @@ const PropertyInsuranceText = withLeftSlideIn(() => {
   );
 });
 
-const HouseInsurance = withSlideDown(() => {
-  return (
-    <div className={styles.card}>
-      <FaHome />
-      <h3>Ubezpieczenie domu</h3>
-    </div>
-  );
-});
+const HouseInsurance = withFadeIn(
+  withSlideDown(() => {
+    return (
+      <div className={styles.card}>
+        <FaHome />
+        <h3>Ubezpieczenie domu</h3>
+      </div>
+    );
+  }, 1500),
+  2000,
+);
 
-const FlatInsurance = withSlideDown(() => {
-  return (
-    <div className={styles.card}>
-      <FaBuilding />
-      <h3>Ubezpieczenie mieszkania</h3>
-    </div>
-  );
-});
+const FlatInsurance = withFadeIn(
+  withSlideDown(() => {
+    return (
+      <div className={styles.card}>
+        <FaBuilding />
+        <h3>Ubezpieczenie mieszkania</h3>
+      </div>
+    );
+  }, 1500),
+  2000,
+);
 
-const HouseInBuildInsurance = withSlideDown(() => {
-  return (
-    <div className={styles.card}>
-      <FaHouseDamager />
-      <h3>Ubezpieczenia domu w budowie</h3>
-    </div>
-  );
-});
+const HouseInBuildInsurance = withFadeIn(
+  withSlideDown(() => {
+    return (
+      <div className={styles.card}>
+        <FaHouseDamage />
+        <h3>Ubezpieczenia domu w budowie</h3>
+      </div>
+    );
+  }, 1500),
+  2000,
+);
 
 export default withScrollIn(PropertyInsurance);
