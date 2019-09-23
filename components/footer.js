692476f465dc37ce3a1b95b2e9cde65cb338e24f
Kamil Jakubus <kamiljkbz@protonmail.com>
2019-09-23 20:53:08 +0200

    Simplify and scale-optimize footer

--- components/footer.js
+++ components/footer.js
@@ -1,48 +1,19 @@
 import styles from '../styles/footer.module.scss'
-import {
-  FaGooglePlusG,
-  FaFacebookF,
-  FaTwitter,
-  FaLinkedinIn,
-  FaMapMarkedAlt,
-} from 'react-icons/fa'
 
 function Footer() {
   return (
     <div className={styles.footer}>
-      <div className={styles.footer__top}>
-        <hr className={styles.footer__line} />
+      <div className={styles.footerTop}>
+        <div className={styles.footerLine} />
         <img src="/static/footer_logo2.png" />
-        <hr className={styles.footer__line} />
+        <div className={styles.footerLine} />
       </div>
-      <div className={styles.footer__mid}>
-        <div className={styles.footer__left}>
-          <h1>
-            biuro@wuemfinanse.pl <br />
-            +48 509 755 700
-          </h1>
-        </div>
-        <hr />
-        <div className={styles.footer__right}>
-          <div className={styles.list__container}>
-            <h2>Ubezpieczenia</h2>
-            <ul>
-              <li>Ubezpieczenia pojazdów</li>
-              <li>Ubezpieczenia zdrowotne</li>
-              <li>Ubezpieczenia turystyczne</li>
-              <li>Ubezpieczenia nieruchomości</li>
-            </ul>
-          </div>
-          <div>
-            <h2>Finanse</h2>
-            <ul>
-              <li>Kredyt</li>
-              <li>Leasing</li>
-            </ul>
-          </div>
-        </div>
+      <div className={styles.footerContent}>
+        <p>Kontakt</p>
+        <p>Polityka prywatnosci</p>
+        <p>RODO</p>
+        <p>© 2019 WueMFinanse.pl</p>
       </div>
-      <div className={styles.footer__bottom}>Copyright © 2019 WueMFinanse</div>
     </div>
   )
 }
